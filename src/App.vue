53a6776a072a0089427b0165def2b179e6af2b14
Asikul Islam Sazzat <asikulislamsazzat@gmail.com>
2024-07-29 09:52:08 +0600

    Bootstrap 5 setup

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,20 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <h3 class="text-danger">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, cupiditate sunt.
+      Molestiae perferendis minus deleniti vel aliquam consequuntur optio ipsam? Itaque
+      ipsum nihil libero sunt perferendis placeat, eligendi consectetur totam suscipit,
+      aperiam magni ex in nemo laborum sed quaerat reprehenderit corrupti sapiente
+      corporis! Excepturi, ab illo consequatur aut necessitatibus tempore voluptatem quis
+      doloribus sapiente deleniti quae exercitationem veniam facere atque sint nihil
+      ullam? Minima temporibus optio facere in, nostrum, quae necessitatibus consectetur
+      fuga quisquam odit error accusamus enim saepe culpa, vero voluptas. Dolorem rerum
+      nulla unde non doloribus? A asperiores quasi esse qui tempore natus porro sapiente
+      iusto quisquam omnis!
+    </h3>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
+/* Custom CSS Link */
+import "./assets/sass/styles.scss";
 
-const app = createApp(App)
+const app = createApp(App);
+app.use(createPinia());
+app.mount("#app");
 
-app.use(createPinia())
-
-app.mount('#app')
+/*Import All of Bootstrap JS */
+import * as bootstrap from "bootstrap";
