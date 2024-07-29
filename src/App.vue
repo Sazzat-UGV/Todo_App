09da0a282df14092c832b82f895205d2d2a52c20
Asikul Islam Sazzat <asikulislamsazzat@gmail.com>
2024-07-29 10:54:40 +0600

    Fetch All Todo and Show in Loop

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,17 @@
-<script setup></script>
+<script setup>
+/*Import All Library */
+import { onMounted } from "vue";
+import { useTodoStore } from "./stores/todo.js";
+
+/*Instance and Variables*/
+const todoStore = useTodoStore();
+
+/*Methods*/
+/*Hooks, Computed, watch*/
+onMounted(() => {
+  todoStore.getAllTodos();
+});
+</script>
 
 <template>
   <div class="container">
@@ -22,8 +35,14 @@
           <div class="card-body">
             <h4 class="text-danger">Tasks</h4>
             <ul class="list-group">
-              <li class="list-group-item d-flex justify-content-between align-items-center">
-                List Item
+              <li
+                v-for="todo in todoStore.todos"
+                :key="todo.id"
+                class="list-group-item d-flex justify-content-between align-items-center"
+              >
+                <span :class="todo.completed ? 'text-decoration-line-through' : ''">
+                  {{ todo.title }}
+                </span>
                 <a href="" class="btn"> <i class="fa-solid fa-xmark"></i></a>
               </li>
             </ul>
